636a88f32cc472c8dc013f9466f198fd37a4552a
calumice <99gastgamer81@gmail.com>
2021-08-26 15:25:36 +0900

    モデルの座標が変わったことによる飛び込み前の座標の変更

--- SwimmingGame/MyDxLibGame3D/FishManager.cpp
+++ SwimmingGame/MyDxLibGame3D/FishManager.cpp
@@ -58,17 +58,17 @@
 /// </summary>
 FishManager::FishManager()
 	:mSourceModelHandle(-1)
-	, BEFORE_DIVING_POS{ VGet(1.5f,15.0f,-28.0f)    //1
-						,VGet(12.0f,9.0f,-28.0f)	//2
-						,VGet(-7.0f,20.0f,26.0f)	//3
-						,VGet(-2.0f,15.0f,28.0f)	//4
-						,VGet(-12.0f,9.0f,29.0f)	//5
-						,VGet(-16.0f,20.0f,-7.0f)	//6
-						,VGet(-17.0f,15.0f,-2.0f)	//7
-						,VGet(-17.0f,9.0f,-12.0f)	//8
-						,VGet(16.0f,20.0f,7.0f)     //9
-						,VGet(17.0f,15.0f,2.0f)	    //10
-						,VGet(17.0f,9.0f,12.0f) }	//11
+	, BEFORE_DIVING_POS{ VGet(-5.0f,18.0f,-22.0f)   //1
+						,VGet(5.0f,11.0f,-22.0f)	//2
+						,VGet(5.0f,18.0f,22.0f)		//3
+						,VGet(0.0f,23.0f,20.0f)		//4
+						,VGet(-5.0f,11.0f,22.0f)	//5
+						,VGet(-12.0f,18.0f,5.0f)	//6
+						,VGet(-10.0f,23.0f,0.0f)	//7
+						,VGet(-12.0f,11.0f,-5.0f)	//8
+						,VGet(12.0f,18.0f,-5.0f)    //9
+						,VGet(10.0f,23.0f,0.0f)	    //10
+						,VGet(12.0f,11.0f,5.0f) }	//11
 	, BEFORE_DIVING_ROTATE{	VGet(0.0f, 90.0f * DX_PI_F / 180.0f, 0.0f)    //1
 							,VGet(0.0f,90.0f * DX_PI_F / 180.0f,0.0f)     //2
 							,VGet(0.0f,-90.0f * DX_PI_F / 180.0f,0.0f)    //3
